526ab6f8acc3f6545ffda6376143b150bfd3090d
Sebastian <sebastian.gallard.13@sansano.usm.cl>
2021-04-29 21:53:44 -0400

    piano keys added

---
+++ src/components/Note.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="['note', noteStyle]">
+     {{ noteName }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Note',
+  props: {
+      noteName: String,
+  },
+  computed: {
+    noteStyle() {
+      if (this.noteName.includes('#') || this.noteName.includes('b')) {
+        return 'black'
+      }
+      return 'white'
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+.note {
+  height: calc(var(--width) * 4);
+  width: var(--width);
+}
+
+.white {
+  --width: 100px;
+  background-color: white;
+  border: 1px solid #333;
+  z-index: -1;
+}
+
+.black {
+  --width: 60px;
+  background-color: black;
+  margin-left: -30px;
+  margin-right: -30px;
+  z-index: 2;
+  position: absolute;
+}
+
+</style>
--- src/components/Piano.vue
+++ src/components/Piano.vue
@@ -1,21 +1,43 @@
 <template>
-  <div>
-     {{ keys }}
+  <div class="container">
+    <div v-bind:key="note" v-for="note in keysFromStartToEnd(0,61)">
+      <note :note-name="note"/>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Note from './Note.vue';
 
 export default {
   name: 'Piano',
+  components: {
+    Note,
+  },
   computed: {
     ...mapGetters(['keys']),
+  },
+  methods: {
+    keysFromStartToEnd(start, end) {
+      return this.keys.slice(start, end)
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+*, *::before, *::after {
+  box-sizing: border-box;
+}
 
+.container {
+  display: flex;
+  margin: 0;
+  background-color: blue;
+  padding: 10px;
+  justify-content: center;
+  align-content: center;
+}
 </style>
